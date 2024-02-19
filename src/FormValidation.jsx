a03a7b1fa0655442f038c7c4fdc3a61465fb93df
codetraffics <eboat086@gmail.com>
2024-02-19 07:45:08 -0900

    form validation

---
+++ src/FormValidation.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import "./style.css";
+
+const FormValidation = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [errorUserName, setErrorUserName] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorConfirmPassword, setErrorConfirmPassword] = useState("");
+
+  const [userColor, setUserColor] = useState("");
+  const [emailColor, setEmailColor] = useState("");
+  const [passwordColor, setPasswordColor] = useState("");
+  const [confirmPasswordColor, setConfirmPasswordColor] = useState("");
+
+  const validate = (e) => {
+    e.preventDefault();
+
+    if (userName.length > 8) {
+      setErrorUserName("");
+      setUserColor("green");
+    } else {
+      setErrorUserName("Username must be 8 characters long");
+      setUserColor("red");
+    }
+
+    if (email.includes("@")) {
+      setErrorEmail("");
+      setEmailColor("green");
+    } else {
+      setErrorEmail("Email must contain @ symbol");
+      setEmailColor("red");
+    }
+
+    if (password.length > 8) {
+      setErrorPassword("");
+      setPasswordColor("green");
+    } else {
+      setErrorPassword("Password must be more than 8 characters long");
+      setPasswordColor("red");
+    }
+
+    if (password != "" && password == confirmPassword) {
+      setErrorConfirmPassword("");
+      setConfirmPasswordColor("green");
+    } else {
+      setErrorConfirmPassword("Passwords don't match");
+      setConfirmPasswordColor("red");
+    }
+  };
+
+  return (
+    <>
+      <div className="card">
+        <div className="card-image"></div>
+
+        <form>
+          <input
+            type="text"
+            placeholder="Name"
+            style={{ borderColor: userColor }}
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <p className="error">{errorUserName}</p>
+
+          <input
+            type="text"
+            placeholder="Email"
+            style={{ borderColor: emailColor }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <p className="error">{errorEmail}</p>
+
+          <input
+            type="password"
+            placeholder="Password"
+            style={{ borderColor: passwordColor }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="error">{errorPassword}</p>
+
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            style={{ borderColor: passwordColor }}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <p className="error">{errorConfirmPassword}</p>
+
+          <button className="submit-btn" onClick={validate}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FormValidation;
